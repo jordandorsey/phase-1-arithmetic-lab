352abf1a6bcaba253928c9cd346f5903ae04b3b5
JordanDorsey <jordandorsey04@gmail.com>
2023-09-07 21:01:21 -0400

    index.js

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let num1 = 31
+let num2 = 2
+const multiply = num1 * num2
 
-// Write your code here
+console.log(num1 * num2)
+
+let random = 1
+
+let num3 = 22
+let num4 = 6
+const mod = num3 % num4
+
+console.log(num3 % num4)
+
+let max = 20
